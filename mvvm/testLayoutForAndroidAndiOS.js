302aa8fe94c712d333c2843577332b68d40bab9c
minsavage <minsavage@gmail.com>
2016-04-23 15:01:23 +0800

    add source dir, support npm

---
+++ mvvm/testLayoutForAndroidAndiOS.js
@@ -0,0 +1,14 @@
+/**
+ * Created by danney on 16/4/14.
+ */
+var layout = {
+    content: [{
+        type: 'RelativeView',
+        id: 'relative',
+        style: {
+            width: 'match_parent',
+            height: 'match_parent',
+            alignParentTop: true
+        }
+    }]
+}
